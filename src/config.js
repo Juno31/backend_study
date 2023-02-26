2c72f6224b076d6ff68bfd47800d8071d50315b2
Juno <dghsum@gmail.com>
2023-02-26 18:26:11 +0900

    [Add] App 설정 파일 추가

---
+++ src/config.js
@@ -0,0 +1,17 @@
+const DB_CONFIG = {
+    host: 'localhost',
+    user: 'root',
+    password: '990301aa'
+}
+
+const appConfig = {
+    env: process.env.NODE_ENV,
+    DB_CONFIG: {
+        host: 'localhost',
+        user: 'root',
+        password: '990301aa'
+    },
+    port: 3000
+}
+
+module.export  = DB_CONFIG
